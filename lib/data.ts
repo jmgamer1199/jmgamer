979aa7ef859c378be9fdcabd0945056d9b86de21
jmgamer1199 <jmgamer1199@gmail.com>
2025-08-02 22:48:52 +0200

    Se ha cambiado el formato de imagen de .PNG a .WEBP para mejor rendimiento al cargar en la web

--- lib/data.ts
+++ lib/data.ts
@@ -13,7 +13,7 @@ export const PROJECTS = [
     description: "Este texturepack añade pocas cosas por ejemplo un nuevo fondo de menu y algunas pocas texturas y nombres similares al de java",
     tags: ["json", "Minecraft Texture Pack", "Bedrock Edition"],
     // --- NUEVO ---
-    imageUrl: "/images/texturepack.png", // Ruta a la imagen del proyecto
+    imageUrl: "/images/texturepack.webp", // Ruta a la imagen del proyecto
     // --- NUEVO Y OPCIONAL ---
     downloads: [
       { label: "Descargar para Bedrock", url: "/downloads/JmTexturepack(Bedrock).rar" },
@@ -25,7 +25,7 @@ export const PROJECTS = [
     description: "¡Adéntrate en una aventura inusual con  New Super Mario Bros. J ! Descubre el misterio de la enigmática J que ha aparecido en el primer nivel y prepárate para un viaje donde lo familiar se vuelve extrañamente nuevo. ¿Estás listo para desentrañar el secreto de la J?",
     tags: ["NSMB Editor", "Romhack", "Nintendo DS"],
     // --- NUEVO ---
-    imageUrl: "/images/mario_romhack.png",
+    imageUrl: "/images/mario_romhack.webp",
     downloads: [
       { label: "Descargar", url: "/downloads/New super mario brosJM.rar" },
     ],
@@ -36,7 +36,7 @@ export const PROJECTS = [
     description: "¡Dale la bienvenida a RusAddon a tu mundo de Minecraft Bedrock! Adopta a Rus, un fiel perro con su propia cama, pelota, hueso y cuenco de comida. ¡Disfruta de la compañía de tu nuevo amigo peludo en tus aventuras!",
     tags: ["Json", "Minecraft Addon", "Bedrock Edition"],
     // --- NUEVO ---
-    imageUrl: "/images/rus_addon.png",
+    imageUrl: "/images/rus_addon.webp",
     downloads: [
       { label: "Descargar", url: "/downloads/RusAddon.mcaddon" },
     ],
@@ -47,7 +47,7 @@ export const PROJECTS = [
     description: "Renueva tu entorno de programación con Jm-Theme, un tema para Visual Studio Code que cambia sutilmente tu fondo para una nueva perspectiva. Experimenta cómo un simple ajuste de color puede inspirar tu productividad. Dale una J de giro a tu espacio de trabajo.",
     tags: ["Json", "Visual Studio Code", "Tema"],
     // --- NUEVO ---
-    imageUrl: "/images/jm_theme.png",
+    imageUrl: "/images/jm_theme.webp",
     liveUrl: "https://marketplace.visualstudio.com/items?itemName=Jmgamer1199.jm-theme",
     // Este proyecto no tiene 'downloadUrl', por lo que no mostrará el botón.
   },
@@ -55,7 +55,7 @@ export const PROJECTS = [
     title: "Pokeball 3d",
     description: "Aquí está mi modelo 3D de una Poké Ball; fue mi primer intento y aún estoy aprendiendo a texturizar, por lo que verás algunos errores. Pero estoy orgulloso de la forma, y creo que la esencia de la Poké Ball está ahí.",
     tags: ["Blender", "Photoshop", "3D Model"],
-    imageUrl: "/images/pokeball_reflejos.png",
+    imageUrl: "/images/pokeball_reflejos.webp",
     downloads: [
       { label: "Descargar", url: "/downloads/Pokeball.blend" },
     ],
@@ -64,7 +64,7 @@ export const PROJECTS = [
     title: "Generador QR",
     description: "Crea códigos QR al instante desde tu terminal con esta sencilla app de Python. Introduce tu URL y genera un código QR que se guarda como imagen. Rápido, eficiente y fácil de usar.",
     tags: ["Python", "QR Code", "Generador"],
-    /* imageUrl: "/images/pokeball_reflejos.png", */
+    /* imageUrl: "/images/pokeball_reflejos.webp", */
     repoUrl: "https://github.com/jmgamer1199/QR-code-generator.git",
   },
   
